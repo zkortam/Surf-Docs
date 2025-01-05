0a3b6d7bc70a8115dec57255b23a50d2e7e6fba1
Zakaria Kortam <65579842+zkortam@users.noreply.github.com>
2025-01-04 23:37:02 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
 
   const bannerBackgroundStyle = {
-    backgroundColor: 'var(--ifm-background-color)',
+    backgroundColor: 'var(--ifm-background-color)', // Adapts to light or dark mode
     color: 'var(--ifm-color-text-primary)',
     height: '100vh',
     display: 'flex',
